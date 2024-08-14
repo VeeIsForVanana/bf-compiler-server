d7a4ce29fadd8dcacf12495ea7cd12afa9a0ad77
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-08-15 03:18:12 +0800

    feat: grab old server code from previous attempt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs::File, io::Read, net::TcpListener};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(_) = File::open("exercise_2") {
+        panic!(
+            "Compiler executable 'exercise_2' not found in path {}!",
+            env!("PWD")
+        )
+    }
+    let listener = TcpListener::bind("localhost:0").unwrap();
+    println!("Connect to {}", listener.local_addr().unwrap());
+
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            println!("Established connection to {}", stream.peer_addr().unwrap());
+            let mut req_buf: Vec<u8> = vec![];
+            stream.read_to_end(&mut req_buf).unwrap();
+            let req_str: String = String::from_utf8(req_buf).expect("Expected valid UTF8");
+            let request = parse_request(req_str);
+            println!("{:?}", request);
+        }
+    }
+}
+
+fn parse_request(req_str: String) -> Result<(), String> {
+    let mut lines = req_str.split('\n');
+    let mut first_line = lines.next().unwrap().split(" ");
+    assert!(first_line.clone().count() == 3);
 }
